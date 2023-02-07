8d26d75b45a7f25e139faf6c36d5bb77536560d0
mfrankko <68924207+mfrankko@users.noreply.github.com>
2023-02-07 14:34:53 -0300

    Ajustes

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
+import tkinter as tk
+from tkinter import ttk
 
-dir_path = 'C:/Users/sfran/OneDrive/Documents/Excel/DCTFs'
+root = tk.Tk()
+root.title("Extração DCTF")
+root.geometry('400x100')
+def get_inputs():
+    global pasta1, pasta2
+    pasta1 = pasta1_.get()
+    pasta2 = pasta2_.get()
+    return pasta1, pasta2
+
+label1 = ttk.Label(root, text="Pasta de Origem:")
+label1.pack()
+
+pasta1_ = tk.Entry(root, width=60)
+pasta1_.pack()
+
+label2 = ttk.Label(root, text="Pasta de Destino:")
+label2.pack()
+
+pasta2_ = tk.Entry(root, width=60)
+pasta2_.pack()
+
+submit = tk.Button(root, text="Enviar", command=lambda: [get_inputs(), root.quit()])
+submit.pack()
+
+root.mainloop()
+
+dir_path = pasta1
+dir_destino = pasta2
 html_list = []
 for filename in os.listdir(dir_path):
      if filename.endswith('.html'):
@@ -10,13 +39,12 @@ for filename in os.listdir(dir_path):
 
 dctf = []
 def extracao_dctf(html):
-    f = open(f'C:\\Users\\sfran\\OneDrive\\Documents\\Excel\\DCTFs\\{html}')
+    f = open(f'{dir_path}/{html}')
     soup = BeautifulSoup(f, 'html.parser')
     tables = soup.findAll("table")
 
     codigos = ['5856-01', '2484-01', '5993-01', '6912-01', '0561-07', '1708-06', '0422-01', '1708-06', '0588-06'] #Lista de Códigos da RFB
     table_matrix = [] #Lista onde fica as informações extraídas do HTML num primeiro momento
-    #dctf = [] #Lista onde fica os valores finais extraídos pela função get_index
 
     for table in tables:
         list_of_rows = []
@@ -48,5 +76,5 @@ def extracao_dctf(html):
 extracao = [extracao_dctf(html) for html in html_list]
 df = pd.DataFrame(dctf)
 df.columns = ['Código', 'Débito Apurado', 'Pagamento', 'Compensação']
-df.to_excel('C:/Users/sfran/OneDrive/Documents/Excel/DCTFs/Compliance Obrigações Acessórias.xlsx', index=False)
+df.to_excel(f'{dir_destino}/base.xlsx', index=False)
 
